db8237e12f75fc2ebfbc6fbe15066e7e2609e991
mal1s1sc0de <116841917+mal1s1sc0de@users.noreply.github.com>
2022-10-29 20:49:52 +0300

    Delete src directory

--- src/cmTypes.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef CM3D_TYPES
-#define CM3D_TYPES
-
-#include "BasicTypes.h"
-#include "Scalar.hpp"
-#include "Vector3.hpp"
-
-#endif // CM3D_TYPES
--- src/stdcm3d.hpp
+++
@@ -1,4 +0,0 @@
-#include "World.hpp"
-#include "SbBody.hpp"
-#include "AppOverlay.hpp"
-#include "StdCamera.hpp"
